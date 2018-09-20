c1250600ce2d0481985ce2871ec74c50506cc883
Maksim Astravukh <astravukh@gmail.com>
2018-09-20 08:19:53 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  var hash = {};
+  for(el of array){
+    hash[el] = el;
+  }
+  var result = 0;
+  for(el of array){
+    if(hash[el + 1] !== undefined) continue;
+    let counter = 0;
+    while(hash[el] !== undefined){
+      counter++;
+      el--;      
+    }
+    result = counter>result?counter:result;
+  }
+  return result;
 }
